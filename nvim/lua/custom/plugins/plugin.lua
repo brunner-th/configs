162c23be19c440732a83f29be348b6c00df59eaf
brunner-th <brunner.th@hotmail.com>
2025-01-18 21:13:04 +0100

    updated nvim config

--- nvim/lua/custom/plugins/plugin.lua
+++ nvim/lua/custom/plugins/plugin.lua
@@ -2,6 +2,25 @@ return {
 	-- Other plugins ...
 
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+	},
+
+	{
+		"nvim-tree/nvim-web-devicons",
+		config = function()
+			require("nvim-web-devicons").setup({})
+		end,
+	},
+
+	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons", -- Optional for file icons
@@ -11,20 +30,17 @@ return {
 				-- Add your preferred configuration here
 				view = {
 					width = 30,
-					side = "left",
+					side = "right",
 				},
 				renderer = {
-					icons = {
-						{
-							file = true,
-							folder = true,
-							folder_arrow = true,
-							git = true,
-						},
-						glyphs = {
-							default = "-",
-						},
-					},
+					--icons = {
+					--{
+					--	file = true,
+					--	folder = true,
+					--	folder_arrow = true,
+					--	git = true,
+					--},
+					--},
 				},
 			})
 		end,
